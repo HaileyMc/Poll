034c1886a5a678a52d47af6d6cc1636185fc3a05
HaileyMc <71082916+HaileyMc@users.noreply.github.com>
2021-04-12 12:28:22 -0500

    Update poll.js

--- poll.js
+++ poll.js
@@ -1,13 +1,14 @@
-(function (global, factory) {
+function notAnIIFE(global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('@bhmb/bot')) :
     typeof define === 'function' && define.amd ? define(['@bhmb/bot'], factory) :
     (factory(global['@bhmb/bot']));
-}(this, (function (bot) { 'use strict';
+}
+
+function factoryFunction(bot) {
+    'use strict';
     const MessageBot = bot.MessageBot
 
- MessageBot.registerExtension('hailey/poll', function (ex, world) {
-         ex.world.addCommand('/poll', function() {
+    MessageBot.registerExtension('hailey/poll', function (ex, world) {
+        ex.world.addCommand('/poll', function() {
         ex.bot.send('Vote counted!');
-    });});
-    
-                        })));
+}
